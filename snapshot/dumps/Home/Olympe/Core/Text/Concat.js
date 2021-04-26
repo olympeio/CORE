14bb42dea7c08366fc6ad293010c40dec97c9007
Jean-Christophe Collet <jean-christophe.collet@olympe.ch>
2021-04-26 15:26:49 +0200

    Fixed Concat brick compatibility

--- snapshot/dumps/Home/Olympe/Core/Text/Concat.js
+++ snapshot/dumps/Home/Olympe/Core/Text/Concat.js
@@ -39,7 +39,7 @@ const _Runnable_Definition_Outputs_01621fb84e320000f00b = '01621fb84e320000f00b'
 const _Runnable_IO_Type_IO_0168a431d3b9b7438424     = '0168a431d3b9b7438424';
 
 
-// Definition of tags used for newly created instances 
+// Definition of tags used for newly created instances
 const _instance_of_Coded_Function_model____Concat_01626be2f8325144b551 = db.assignTag('01626be2f8325144b551');
 const _instance_of_Runnable_Input____s1_01626be4ad8f74117191 = db.assignTag('01626be4ad8f74117191');
 const _instance_of_Runnable_Input____s2_01626be5410d4bc19491 = db.assignTag('01626be5410d4bc19491');
@@ -65,13 +65,13 @@ const _Text_0178445ee2a56bf00fd6               = '0178445ee2a56bf00fd6';
 
 
 // :_instance_of_Coded_Function_model____Concat_01626be2f8325144b551
-db.newModel(_instance_of_Coded_Function_model____Concat_01626be2f8325144b551, 'Concat') // tag and name of the model 
-    .extends(_Function_Call_0168c6f020f2a96f0000).setModelTag(_Coded_Function_01621fb84e320000f011) 
+db.newModel(_instance_of_Coded_Function_model____Concat_01626be2f8325144b551, 'Concat') // tag and name of the model
+    .extends(_Function_Call_0168c6f020f2a96f0000).setModelTag(_Coded_Function_01621fb84e320000f011)
     .setProperties({
         [_Runnable_Definition_hardcoded_01692aa71f3108454479]:true,
         'v'                                     :1
     })
-    .done(); 
+    .done();
 db.setRootInstanceTag(_instance_of_Coded_Function_model____Concat_01626be2f8325144b551);
 // :_instance_of_Runnable_Input____s1_01626be4ad8f74117191
 db.newInstance(_instance_of_Runnable_Input____s1_01626be4ad8f74117191)
@@ -101,7 +101,7 @@ db.newInstance(_instance_of_Runnable_Output____s1$_43_$s2_01626be5ba6b26eb2aa0)
     // Tag of goos
     .addToGroupOfObjects(_instance_of_Coded_Function_model____Concat_01626be2f8325144b551)
     .setNameOrProperties({
-        [_Object_name_ff023000000000000011]     :'s1+s2',
+        [_Object_name_ff023000000000000011]     :'s1 + s2',
         [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:0,
         'v'                                     :1
     })
