59cb14a530ea1cb5851306cad6d5343f653f8e0b
Jean-Christophe Collet <jean-christophe.collet@olympe.ch>
2021-05-03 10:11:45 +0200

    CI: publish packages on develop

---
+++ bumpVersion.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export PACKAGE='@olympeio/core'
+export VERSION=$(jq -rc '.version' ./package.json)
+export BASE=$(npm view "$PACKAGE" versions | grep "${VERSION}." | sed -e "s/^.*${VERSION}\.\([0-9][0-9]*\).*$/\1/" | sort -n | tail -1)
+if [ -z "${BASE}" ];
+then
+  INC=0
+else
+  INC=$((BASE + 1))
+fi
+export PUBLISH_VER="${VERSION}.${INC}"
+
+echo "Setting npm packages version to ${PUBLISH_VER}"
+sed -i -e "s/\"version\": \".*\",/\"version\": \"${PUBLISH_VER}\",/" package.json
+
+
