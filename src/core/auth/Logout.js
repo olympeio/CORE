fef12b9395fe4c7d640e08af4c0de1775f5bb264
Andrea Veneziano <andrea.veneziano@olympe.ch>
2021-05-31 15:13:46 +0200

    Migrate Logout brick

---
+++ src/core/auth/Logout.js
@@ -0,0 +1,29 @@
+
+import { ActionBrick, registerBrick, Auth, AuthState } from 'olympe';
+
+/**
+
+**/
+export default class Logout extends ActionBrick {
+
+    /**
+     * Executed every time an input gets updated.
+     * Note that this method will _not_ be executed if an input value is undefined.
+     *
+     * @protected
+     * @param {!Context} context
+     * @param {!Array} inputs
+     * @param {!Array} outputs
+     */
+    onUpdate(context, inputs, [dispatchControlFlow]) {
+        if (Auth.getState() === AuthState.AUTHENTICATED) {
+            Auth.logout().then((_) => {
+                if (Auth.getState() === AuthState.GUEST) {
+                    dispatchControlFlow();
+                }
+            });
+        }
+    }
+}
+
+registerBrick('0163d0188add5a16a56d', Logout);
