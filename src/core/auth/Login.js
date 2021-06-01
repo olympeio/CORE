3f1e6c6dfb8e93fab033ed8c4ae93816da95d1e4
Jeremy Smidts <jeremy.smidts@olympe.ch>
2021-06-01 09:31:34 +0200

    Added the Login brick <0163d01778b931d801dd> Tests to be added later when helpers are available.

---
+++ src/core/auth/Login.js
@@ -0,0 +1,65 @@
+import {registerBrick, Context, Auth, AuthState, ErrorFlow, FunctionBrick } from 'olympe'
+
+
+/**
+## Description
+Attempts to log a user in providing its name and password. The authentication is attempted only once when the input control flow is triggered.
+## Inputs
+| Name | Type | Description |
+| --- | :---: | --- |
+| controlFlow | Control Flow | Control flow to trigger the login process |
+| username | String | The username. |
+| password | String | The password for that user. |
+## Outputs
+| Name | Type | Description |
+| --- | :---: | --- |
+| On failure | Control Flow | Control flow triggered on failed authentication because of invalid credentials. |
+| On success | Control Flow | Control flow triggered on successful authentication. |
+| Error Flow | Error Flow | Error flow triggered when the server is unreachable or the authentication fails for unknown reasons. |
+## Errors
+| Code | Description |
+| --- | --- |
+| undefined | undefined | Server unreachable. |
+| undefined | undefined | Authentication error. |
+
+**/
+export default class Login extends FunctionBrick {
+  /**
+   * Executed every time an input gets updated.
+   * Note that this method will _not_ be executed if an input value is undefined.
+   *
+   * @protected
+   * @param {!Context} context
+   * @param {!Array} inputs
+   * @param {!Array} outputs
+   */
+  onUpdate(context, [controlFlow,username,password], [onSuccess,dispatchErrorFlow, onFailure]) {
+    Auth.loginSRP(username, password)
+        .then( () => {
+          const currentState = Auth.getState()
+          switch (currentState) {
+            case AuthState.AUTHENTICATED:
+              onSuccess();
+              break;
+            case AuthState.GUEST:
+              // Invalid credentials
+              onFailure();
+              break;
+            case AuthState.ERROR:
+              // Authentication error
+                dispatchErrorFlow(ErrorFlow.create('Authentication error',2));
+              break;
+            case AuthState.DISCONNECTED:
+              // Server unreachable
+                dispatchErrorFlow(ErrorFlow.create('The server is unreachable', 1));
+              break;
+            default:
+          }
+        })
+        .catch( () => {
+            dispatchErrorFlow(ErrorFlow.create('Authentication error',2));
+        })
+  }
+}
+
+registerBrick('0163d01778b931d801dd', Login)
