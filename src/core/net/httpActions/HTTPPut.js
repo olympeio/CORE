0f06f52224a501c609ac0b6f89cc53c47d2161ae
Jael Dubey <jael.dubey@olympe.ch>
2021-06-01 15:01:21 +0200

    Create HTTP Put brick

---
+++ src/core/net/httpActions/HTTPPut.js
@@ -0,0 +1,66 @@
+
+import { ActionBrick, registerBrick, Context } from 'olympe';
+import doHttpRequest from "../httpUtils/helpers";
+import {ErrorFlow} from "@olympeio/runtime-web";
+
+/**
+ ## Description
+ Sends an HTTP PUT request to the specified URL and provide the results.
+
+ The HTTP PUT request method creates a new resource or replaces a representation of the target resource with the request body.
+ The difference between PUT and POST is that PUT is idempotent: calling it once or several times successively has the
+ same effect (that is no side effect), where successive identical POST may have additional effects, like passing an
+ order several times.
+
+ Additional headers can be provided and returned in a string that has to be in JSON format.
+
+ **Example:** '{"Content-Type": "text/html; charset=UTF-8",  "Content-Length": 1024 }'
+
+ ## Inputs
+ | Name | Type | Description |
+ | --- | :---: | --- |
+ | URL | String | The URL to query. |
+ | Headers | String | Optional HTTP headers in a JSON parsable string. |
+ | Body | String | The body of the request. |
+ ## Outputs
+ | Name | Type | Description |
+ | --- | :---: | --- |
+ | Response Status Code | Number | The response status code. |
+ | Response Status Text | String | The response status text. |
+ | Response Body | String | The body (i.e. the content) of the response. |
+ | Response Headers | String | The response headers. |
+
+ **/
+export default class HTTPPut extends ActionBrick {
+
+    /**
+     * Executed every time an input gets updated.
+     * Note that this method will _not_ be executed if an input value is undefined.
+     *
+     * @protected
+     * @param {!Context} context
+     * @param {!Array} inputs
+     * @param {!Array} outputs
+     */
+    onUpdate(context, [body, headers, url], [ forwardEvent, setErrorFlow, setStatusCode, setHeaders]) {
+        doHttpRequest('PUT', url, headers)
+            .then(_response => {
+                    if (_response.ok) {
+
+                        let headersResponse = {};
+                        for (const pair of _response.headers.entries()) {
+                            headersResponse[pair[0]] = pair[1];
+                        }
+                        setHeaders(JSON.stringify(headersResponse));
+
+                    } else {
+                        setErrorFlow(ErrorFlow.create('Network error', _response.status));
+                    }
+                    setStatusCode(_response.status);
+                    forwardEvent();
+                }
+            );
+    }
+}
+
+registerBrick('016856cc8830ae0edc2c', HTTPPut);
