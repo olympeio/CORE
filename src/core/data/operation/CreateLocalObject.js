95b9308677caf0e1ea124ac5d8b7b4169ceaf429
Bertrand Le Foulgoc <bertrand.lefoulgoc@olympe.ch>
2021-06-01 13:17:09 +0000

    create local object

---
+++ src/core/data/operation/CreateLocalObject.js
@@ -0,0 +1,65 @@
+import { FunctionBrick, registerBrick, Context, DBView, instanceToTag, Transaction, Sync } from 'olympe';
+
+/**
+## Description
+Creates an instance of the specified model. It is said to be `local` because it is not yet persisted in the main
+database. See `Persist Object` for how to persist in the database a newly created local object.
+## Inputs
+| Name | Type | Description |
+| --- | :---: | --- |
+| model | Model | The model of the instance. |
+## Outputs
+| Name | Type | Description |
+| --- | :---: | --- |
+| object | Object | The created instance. |
+
+**/
+export default class CreateLocalObject extends FunctionBrick {
+
+    /**
+     * Executed every time an input gets updated.
+     * Note that this method will _not_ be executed if an input value is undefined.
+     *
+     * @protected
+     * @param {!Context} context
+     * @param {!Array} inputs
+     * @param {!Array} outputs
+     */
+    onUpdate(context, [model], [setObject]) {
+        // start isolated local transaction
+        const transaction = new Transaction();
+        transaction.persist(false);
+
+        // And create operation to the transaction
+        const instanceTag = transaction.create(model).persist(false).getTag();
+
+        // Execute the transaction
+        transaction.execute((success) => {
+            if (!success) {
+                console.error('CreateLocalObject: isolated transaction (local) failed');
+            } else {
+                setObject(Sync.getInstance(instanceTag));
+            }
+        });
+
+        // Destroy the local object when context is unloaded
+        // WARNING: we register this callback independently if the Create transaction is performed by this "brick"
+        //              or by an "end" transaction brick
+        context.onDestroy(() => {
+            // destroy the local object :
+            //  - if it was created (if it was not the source should be undefined)
+            //  - if it was not persisted (its source is self only)
+            if (DBView.exist(instanceTag) && !DBView.isPersisted(instanceTag)) {
+                const reverseTransaction = new Transaction();
+                reverseTransaction.delete(instanceTag);
+                reverseTransaction.execute(success => {
+                    if (!success) {
+                        console.error('CreateLocalObject: failed to delete local object. Investigate.');
+                    }
+                });
+            }
+        });
+    }
+}
+
+registerBrick('016778ac951d2b536b23', CreateLocalObject);
