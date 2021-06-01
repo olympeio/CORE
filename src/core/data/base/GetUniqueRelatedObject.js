61d64ed339dfa7d715c8b19468eea68655f34a20
Grégory Moinat <gregory.moinat@olympe.ch>
2021-06-01 16:31:40 +0200

    Get Related Objects

--- src/core/data/base/GetUniqueRelatedObject.js
+++ src/core/data/base/GetUniqueRelatedObject.js
@@ -1,4 +1,5 @@
-import { FunctionBrick, registerBrick, Sync, DBView, RelationPrimitive, Direction, ListDef, instanceToTag, transformers } from 'olympe';
+import { FunctionBrick, registerBrick, Sync, RelationPrimitive, Direction, ListDef, instanceToTag, transformers } from 'olympe';
+import {getRelatedObjects} from "../list/GetRelatedObjects";
 
 /**
 ## Description
@@ -27,29 +28,21 @@ export default class GetUniqueRelatedObject extends FunctionBrick {
      * @param {function(Sync)} setObject
      */
     onUpdate(context, [object, relation], [setObject]) {
-        // Prevent errors if an object with an invalid type was provided
-        if (instanceToTag(object) === '' || instanceToTag(relation) === '') {
-            console.error('GetUniqueRelatedObject: Invalid type provided as input');
+        // Prevent errors
+        if (instanceToTag(object) === '') {
+            console.error('Get Related Objects: Invalid `object` provided');
+            return;
+        } else if (instanceToTag(relation) === '') {
+            console.error('Get Related Objects: Invalid `relation` provided');
             return;
         }
 
-        const db = DBView.get();
-
-        // NB Relation here is a instance of Relation
-        // Get model at origin of relation
-        const originModel = db.getUniqueRelated(relation, RelationPrimitive.originModelRel);
-
-        // Auto detect the direction in which the relation should be followed : assess whether the provided object is an instance of the relation's origin
-        // In case the relation as the same model as origin and destination, the only supported behaviour is origin -> destination (COM-1042)
-        const direction = originModel === db.model(object) ? Direction.DESTINATION : Direction.ORIGIN;
-
-        const orientedRelation = new transformers.Related(relation, direction);
-        const relatedObjects = new ListDef(object, orientedRelation);
+        const relatedObjects = getRelatedObjects(object, relation);
         relatedObjects.observeSize().subscribe((size) => {
             if (size.value > 1) {
                 console.warn(`${db.name(db.model(object))} ${instanceToTag(object)} is related to ${size.value} objects through relation ${db.name(relation)}`);
             }
-        })
+        });
         relatedObjects.observeFirst().subscribe(setObject);
     }
 }
