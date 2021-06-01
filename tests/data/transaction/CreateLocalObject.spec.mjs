c08de9c990945de0136f100a682868b0b596fd90
Patrick Champion <patrick.champion@olympe.ch>
2021-06-01 10:02:55 +0200

    CreateLocalObject

---
+++ tests/data/transaction/CreateLocalObject.spec.mjs
@@ -0,0 +1,15 @@
+import CreateLocalObject from '../../../src/core/data/transaction/CreateLocalObject.js';
+
+describe('CreateLocalObject brick', () => {
+    xit('should correctly create a local object', () => {
+        // TODO
+    });
+
+    xit('should not execute if the model is null', () => {
+        // TODO
+    });
+
+    xit('should return the object tag if used inside a Begin/End', () => {
+        // TODO
+    });
+});
