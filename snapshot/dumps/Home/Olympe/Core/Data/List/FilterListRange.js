1878e817060321c89b97bd4491d21e54e1378974
julien muller <julien.muller@olympe.ch>
2021-06-01 14:15:59 +0200

    Filter list range

---
+++ snapshot/dumps/Home/Olympe/Core/Data/List/FilterListRange.js
@@ -0,0 +1,170 @@
+
+// Variable db typed for auto-completion:
+db = /**@type{olympe.dc.io.OSaveFileBuilder}*/(db);
+
+
+// Checks are disabled since this is a automatically generated script issued by the InitScriptBuilder
+db.disableChecks();
+
+
+
+// Definition of tags of models used to construct instances
+let _Coded_Function_01621fb84e320000f011     = '01621fb84e320000f011';
+let _Coded_Action_01621fb84e320000f012       = '01621fb84e320000f012';
+let _Documentation_Function_Metadata_0172c6ae82952a74193c = '0172c6ae82952a74193c';
+let _Function_Call_0168c6f020f2a96f0000      = '0168c6f020f2a96f0000';
+let _Runnable_Input_0168a431d25001540001     = '0168a431d25001540001';
+let _Runnable_Output_0168a431d25001540002    = '0168a431d25001540002';
+let _String_ff021000000000000011             = 'ff021000000000000011';
+
+
+// Definition of tags of defined properties
+let _Documentation_Function_Metadata_errors_0172c6ae82952a74193f = '0172c6ae82952a74193f';
+let _Documentation_Function_Metadata_inputs_0172c6ae82952a74193d = '0172c6ae82952a74193d';
+let _Documentation_Function_Metadata_outputs_0172c6ae82952a74193e = '0172c6ae82952a74193e';
+let _Documentation_Metadata_description_01703376c4a81c0e9d92 = '01703376c4a81c0e9d92';
+let _Documentation_Metadata_toolTip_01703376c4a81c0e9d91 = '01703376c4a81c0e9d91';
+let _Object_name_ff023000000000000011             = 'ff023000000000000011';
+let _Runnable_Definition_hardcoded_01692aa71f3108454479 = '01692aa71f3108454479';
+let _Runnable_IO_IO_Rank_01740c5d4ae4b520dd01     = '01740c5d4ae4b520dd01';
+
+
+// Definition of tags of defined relations
+let _Display_Metadata_$_40_$Runtime$_41_$_documentation_01703376c4a81c0e9d93 = '01703376c4a81c0e9d93';
+let _Folder_itemRel_016a697ef00ce7554692          = '016a697ef00ce7554692';
+let _GOO_parentGooRel_ff02200000000000000u        = 'ff02200000000000000u';
+let _Object_containsRel_ff02200000000000000f      = 'ff02200000000000000f';
+let _Object_gooRel_ff022000000000000003           = 'ff022000000000000003';
+let _Runnable_Definition_Inputs_01621fb84e320000f00a = '01621fb84e320000f00a';
+let _Runnable_Definition_Outputs_01621fb84e320000f00b = '01621fb84e320000f00b';
+let _Runnable_IO_Type_IO_0168a431d3b9b7438424     = '0168a431d3b9b7438424';
+let _Runnable_IO_Default_Value_01740c5d4ae4b520dd02 = '01740c5d4ae4b520dd02';
+
+//==========================================================
+//========= EXTERNAL REFERENCES ============================
+//==========================================================
+let _Math_0178445f8bd56b19894f               = '0178445f8bd56b19894f';
+let _Text_0178445ee2a56bf00fd6               = '0178445ee2a56bf00fd6';
+let _Logic_0178445f8bd56b198950              = '0178445f8bd56b198950';
+let _Auth_0179b2673490430cc604               = '0179b2673490430cc604';
+let _Net_0179b26969330c80c58b                = '0179b26969330c80c58b';
+let _HttpFunctions_0179b2697a2590496bea      = '0179b2697a2590496bea';
+let _HttpActions_0179b269940cee14a13d        = '0179b269940cee14a13d';
+let _Data_0179b2681e7d4704e92a               = '0179b2681e7d4704e92a';
+let _List_0179b2685e214c57ba47               = '0179b2685e214c57ba47';
+let _Transaction_0179b26880c1af51a32b        = '0179b26880c1af51a32b';
+let _Operation_0179b268740d49b2a89d          = '0179b268740d49b2a89d';
+let _JSON_0179b2684a901d133726               = '0179b2684a901d133726';
+let _DataBase_0179b26837d01a7c3cdc           = '0179b26837d01a7c3cdc';
+let _System_0179b269b5c75228d70f             = '0179b269b5c75228d70f';
+let _DateTime_0179b2690f49a1f52652           = '0179b2690f49a1f52652';
+let _File_0179b2692921b321f99d               = '0179b2692921b321f99d';
+let _UI_0179b269c9b393d21bab                 = '0179b269c9b393d21bab';
+let _Navigation_0179b269f70efbab6b8d         = '0179b269f70efbab6b8d';
+let _Event_0179b269e44e8b51bce0              = '0179b269e44e8b51bce0';
+let _Util_0179b26a12e03686de9c               = '0179b26a12e03686de9c';
+
+
+// Definition of tags used for newly created instances
+let _instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43 = db.assignTag('01717d5f60be1607fb43');
+let _instance_of_Runnable_Input____01717d5fa634e9910dde = db.assignTag('01717d5fa634e9910dde'); // End
+let _instance_of_Runnable_Input____01717d5f944ebf8d73aa = db.assignTag('01717d5f944ebf8d73aa'); // Object list
+let _instance_of_Runnable_Input____01717d5f9bfe70ae5790 = db.assignTag('01717d5f9bfe70ae5790'); // Start
+let _instance_of_Runnable_Output____01717d5fb0a46ea7d586 = db.assignTag('01717d5fb0a46ea7d586'); // Sublist
+let _instance_of_Documentation_Function_Metadata_d0c17d5f60be1607fb43 = db.assignTag('d0c17d5f60be1607fb43');
+
+// :_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43
+db.newModel(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, 'Filter List Range').extends(_Function_Call_0168c6f020f2a96f0000).setModelTag(_Coded_Function_01621fb84e320000f011)
+    .setProperties({
+        [_Runnable_Definition_hardcoded_01692aa71f3108454479]:true,
+        'v'                                     :1
+    })
+    .done();
+db.updateInstance(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, {
+        [db.DEFAULT+_Object_name_ff023000000000000011]:'Filter List Range',
+        'v'                                     :2
+    });
+db.setRootInstanceTag(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43);
+
+// :_instance_of_Runnable_Input____01717d5fa634e9910dde - End
+db.newInstance(_instance_of_Runnable_Input____01717d5fa634e9910dde)
+    .setModelTag('0168a431d25001540001') // tag of the model
+    // Tag of goos
+    .addToGroupOfObjects(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43)
+    .setNameOrProperties({
+        [_Object_name_ff023000000000000011]     :'End',
+        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:0,
+        'v'                                     :1
+    })
+    .done();
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____01717d5fa634e9910dde);
+db.assign(_instance_of_Runnable_Input____01717d5fa634e9910dde, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , 'ff021000000000000013');
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____01717d5fa634e9910dde);
+
+// :_instance_of_Runnable_Input____01717d5f944ebf8d73aa - Object list
+db.newInstance(_instance_of_Runnable_Input____01717d5f944ebf8d73aa)
+    .setModelTag('0168a431d25001540001') // tag of the model
+    // Tag of goos
+    .addToGroupOfObjects(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43)
+    .setNameOrProperties({
+        [_Object_name_ff023000000000000011]     :'Object list',
+        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:1,
+        'v'                                     :1
+    })
+    .done();
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____01717d5f944ebf8d73aa);
+db.assign(_instance_of_Runnable_Input____01717d5f944ebf8d73aa, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , '01631aacd8276ea98490');
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____01717d5f944ebf8d73aa);
+
+// :_instance_of_Runnable_Input____01717d5f9bfe70ae5790 - Start
+db.newInstance(_instance_of_Runnable_Input____01717d5f9bfe70ae5790)
+    .setModelTag('0168a431d25001540001') // tag of the model
+    // Tag of goos
+    .addToGroupOfObjects(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43)
+    .setNameOrProperties({
+        [_Object_name_ff023000000000000011]     :'Start',
+        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:2,
+        'v'                                     :1
+    })
+    .done();
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____01717d5f9bfe70ae5790);
+db.assign(_instance_of_Runnable_Input____01717d5f9bfe70ae5790, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , 'ff021000000000000013');
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____01717d5f9bfe70ae5790);
+
+// :_instance_of_Runnable_Output____01717d5fb0a46ea7d586 - Sublist
+db.newInstance(_instance_of_Runnable_Output____01717d5fb0a46ea7d586)
+    .setModelTag('0168a431d25001540002') // tag of the model
+    // Tag of goos
+    .addToGroupOfObjects(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43)
+    .setNameOrProperties({
+        [_Object_name_ff023000000000000011]     :'Sublist',
+        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:0,
+        'v'                                     :1
+    })
+    .done();
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Runnable_Definition_Outputs_01621fb84e320000f00b  , _instance_of_Runnable_Output____01717d5fb0a46ea7d586);
+db.assign(_instance_of_Runnable_Output____01717d5fb0a46ea7d586, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , '01631aacd8276ea98490');
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Output____01717d5fb0a46ea7d586);
+
+// :_instance_of_Documentation_Function_Metadata_d0c17d5f60be1607fb43
+db.newInstance(_instance_of_Documentation_Function_Metadata_d0c17d5f60be1607fb43)
+    .setModelTag(_Documentation_Function_Metadata_0172c6ae82952a74193c) // tag of the model
+    .setNameOrProperties({
+        [_Documentation_Function_Metadata_errors_0172c6ae82952a74193f]: "",
+        [_Documentation_Function_Metadata_inputs_0172c6ae82952a74193d]: "## Inputs\n| Name | Type | Description |\n| --- | :---: | --- |\n| 01717d5f944ebf8d73aa-Object list | List | The list from which to take objects. |\n| 01717d5f9bfe70ae5790-Start | Number | Lower range index. |\n| 01717d5fa634e9910dde-End | Number | Upper range index. |\n",
+        [_Documentation_Function_Metadata_outputs_0172c6ae82952a74193e]: "## Outputs\n| Name | Type | Description |\n| --- | :---: | --- |\n| 01717d5fb0a46ea7d586-Sublist | List | The sublist. |\n",
+        [_Documentation_Metadata_description_01703376c4a81c0e9d92]: "## Description\nGet objects from a list in a specific range. All items with a rank going from `Start` to `End` (limits included) are\nincluded in the output list.\n",
+        [_Documentation_Metadata_toolTip_01703376c4a81c0e9d91]: "Get a sub-list by rank",
+        'v'                                     :1
+    })
+    .done();
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Display_Metadata_$_40_$Runtime$_41_$_documentation_01703376c4a81c0e9d93, _instance_of_Documentation_Function_Metadata_d0c17d5f60be1607fb43);
+db.assign(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Object_containsRel_ff02200000000000000f          , _instance_of_Documentation_Function_Metadata_d0c17d5f60be1607fb43);
+db.assign(_instance_of_Documentation_Function_Metadata_d0c17d5f60be1607fb43, _Object_gooRel_ff022000000000000003, _instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43);
+
+// TODO: Assigning to a Folder. Check this!
+let parentFolder = _List_0179b2685e214c57ba47; // Core/Data/List
+db.assignOptional(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _Object_gooRel_ff022000000000000003, parentFolder);
+db.assignOptional(parentFolder, _Object_containsRel_ff02200000000000000f, _instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43);
+db.assignOptional(_instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43, _GOO_parentGooRel_ff02200000000000000u, parentFolder);
+db.assignOptional(parentFolder, _Folder_itemRel_016a697ef00ce7554692, _instance_of_Coded_Function_model____FilterListRange_01717d5f60be1607fb43);                
