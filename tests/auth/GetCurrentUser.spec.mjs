8fd18abf7369abce2d158c9eb76e338009b5d30d
Jeremy Smidts <jeremy.smidts@olympe.ch>
2021-06-01 14:27:25 +0200

    Added the GetCurrentUser Brick <0163d016ab80564f0d88>

---
+++ tests/auth/GetCurrentUser.spec.mjs
@@ -0,0 +1,7 @@
+import GetCurrentUser from '../..//Users/jeremy/olympe/core/src/core/auth/GetCurrentUser.js'; // TODO: Fix this path
+
+describe('GetCurrentUser brick', () => {
+    it('should do something', () => {
+        // Write your tests here
+    });
+});
