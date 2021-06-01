022a2ab56c2b873029f9ad1e0d160d46d2b43741
Fabien Jolidon <fabien.jolidon@olympe.ch>
2021-06-01 08:58:06 +0000

    Added implementation for GetFirstObjectInList and GetObjectAtInList

---
+++ tests/data/list/GetObjectAtInList.spec.mjs
@@ -0,0 +1,29 @@
+import GetObjectAtInList from '../../../src/core/data/list/GetObjectAtInList.js';
+
+// Mocked ListDef
+class ListDef {
+    getAt() {
+        return null;
+    }
+}
+
+describe('GetObjectAtInList brick', () => {
+    it('should call the getAt method for a listdef', () => {
+        // TODO
+    });
+    it('should return the object at the given rank for a listdef', () => {
+        // TODO
+    });
+    it('should update the object at the given rank for a listdef', () => {
+        // TODO
+    });
+    it('should return the object at the given rank for an array', () => {
+        // TODO
+    });
+    it('should display a warning if the array is smaller than the rank', () => {
+        // TODO
+    });
+    it('should handle the case when the input has an incorrect type gracefully', () => {
+        // TODO
+    });
+});
