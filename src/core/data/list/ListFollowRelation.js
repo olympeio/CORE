603f0585d57ae4e162f4cb8a80c944d7cc0862e0
Patrick Champion <patrick.champion@olympe.ch>
2021-06-01 11:39:14 +0000

    ListFollowRelation

---
+++ src/core/data/list/ListFollowRelation.js
@@ -0,0 +1,48 @@
+
+import {FunctionBrick, registerBrick, Context, ListDef, Relation, instanceToTag, transformers, DBView, Sync} from 'olympe';
+
+/**
+NO DOC
+**/
+export default class ListFollowRelation extends FunctionBrick {
+
+    /**
+     * Executed every time an input gets updated.
+     * Note that this method will _not_ be executed if an input value is undefined.
+     *
+     * @protected
+     * @param {!Context} context
+     * @param {!ListDef|!Array} list
+     * @param {!Relation} relation
+     * @param {function(!ListDef|!Array)} setFlattenedList
+     */
+    onUpdate(context, [list, relation], [setFlattenedList]) {
+        // Guards
+        if(!Array.isArray(list) && !(list instanceof ListDef)) {
+            console.error('[ListFollowRelation] TypeError: the list should be of type ListDef or Array');
+            return;
+        }
+        if(!instanceToTag(relation)) {
+            console.error('[ListFollowRelation] relation is not specified');
+            return;
+        }
+
+        // Transformer
+        let flattenedList = list;
+        if(Array.isArray(list)) {
+            const db = DBView.get();
+            flattenedList = list.reduce((acc, cur) => {
+                for(let tag of db.getRelated(cur, relation))
+                    acc.push(Sync.getInstance(tag));
+                return acc;
+            }, []);
+        } else {
+            flattenedList = list.transform(new transformers.Related(relation, relation.getDirection()));
+        }
+
+        // Done
+        setFlattenedList(flattenedList);
+    }
+}
+
+registerBrick('016b74da62846557a6f3', ListFollowRelation);
