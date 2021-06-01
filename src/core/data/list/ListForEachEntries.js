1be687e83713c033023ea469cf7713eca5140191
Fabien Jolidon <fabien.jolidon@olympe.ch>
2021-06-01 11:21:18 +0200

    Added implementation for ListForEachEntries and ListForEachRemovedEntries

---
+++ src/core/data/list/ListForEachEntries.js
@@ -0,0 +1,38 @@
+
+import {FunctionBrick, registerBrick, Context, ListDef} from 'olympe';
+
+/**
+## Description
+Get all entries of a list. First, get all entries existing before the execution of this function. Then, continuously
+get all new entries added to the watched list.
+## Inputs
+| Name | Type | Description |
+| --- | :---: | --- |
+| List | List | The list from which to get entries. |
+## Outputs
+| Name | Type | Description |
+| --- | :---: | --- |
+| Entry | Object | A single list entry. |
+
+**/
+export default class ListForEachEntries extends FunctionBrick {
+
+    /**
+     * Executed every time an input gets updated.
+     * Note that this method will _not_ be executed if an input value is undefined.
+     *
+     * @protected
+     * @param {!Context} context
+     * @param {!ListDef|!Array} list
+     * @param {function(object)} setObject
+     */
+    onUpdate(context, [list], [setObject]) {
+        if(Array.isArray(list) || list instanceof ListDef) {
+            list.forEach(object => setObject(object));
+        } else {
+            console.error('TypeError: The list should be of type ListDef or Array');
+        }
+    }
+}
+
+registerBrick('01718897a585de88f500', ListForEachEntries);
