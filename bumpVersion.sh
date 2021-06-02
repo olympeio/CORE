85d720fc738b74ef8935410f7d8b7a57b0e2a820
Jean-Christophe Collet <jean-christophe.collet@olympe.ch>
2021-06-02 07:31:27 +0000

    Update bumpVersion.sh [ci skip]

--- bumpVersion.sh
+++ bumpVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PACKAGE='@olympeio/core'
 export VERSION=$(jq -rc '.version' ./package.json)
-export BASE=$(npm view "$PACKAGE" versions | | sed -e 's/,/\n/g' | grep "${VERSION}\." | sed -e "s/^.*${VERSION}\.\([0-9][0-9]*\).*$/\1/" | sort -n | tail -1)
+export BASE=$(npm view "$PACKAGE" versions | sed -e 's/,/\n/g' | grep "${VERSION}\." | sed -e "s/^.*${VERSION}\.\([0-9][0-9]*\).*$/\1/" | sort -n | tail -1)
 if [ -z "${BASE}" ];
 then
   INC=0
