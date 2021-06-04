2f93fbcb5129cb8c493f624e4dd5f8f045447cd5
Andrea Veneziano <andrea.veneziano@olympe.ch>
2021-06-04 07:52:23 +0000

    Migrate DetachAll (now DeleteAllRelations)

---
+++ tests/data/transaction/DeleteAllRelations.spec.mjs
@@ -0,0 +1,11 @@
+import DeleteAllRelations from '../../../src/core/data/transaction/DeleteAllRelations.js';
+
+describe('DeleteAllRelations brick', () => {
+    it('should execute the transaction when it is isolated', () => {
+        // TODO
+    });
+
+    it('should reuse the transaction pushed from a hardcoded context', () => {
+        // TODO
+    });
+});
