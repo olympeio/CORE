3081ef7c05546f9cf52dcc258b262f69697fc6ba
Mathieu Schopfer <mathieu.schopfer@olympe.ch>
2021-06-08 16:46:45 +0200

    Fix `On value` tests

--- tests/helpers/testResults.mjs
+++ tests/helpers/testResults.mjs
@@ -1,4 +1,4 @@
 import { Context } from 'olympe';
 export default function testResults(brick, inputs, expectedOutputsFunctions) {
     brick.onUpdate(new Context(), inputs, expectedOutputsFunctions);
-};
+}
