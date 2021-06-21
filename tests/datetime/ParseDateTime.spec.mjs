df080a79f89bf2278db5627c9bbb0b50a10142b2
Jael Dubey <jael.dubey@olympe.ch>
2021-06-21 12:52:33 +0000

    Fix Parse DateTime brick

--- tests/datetime/ParseDateTime.spec.mjs
+++ tests/datetime/ParseDateTime.spec.mjs
@@ -1,6 +1,7 @@
 import ParseDateTime from '../../src/core/datetime/ParseDateTime.js';
 import testEqual from '../helpers/testEqual.mjs';
 import { parse, parseISO } from 'date-fns';
+import {Context} from 'olympe';
 
 describe('ParseDateTime brick', () => {
     it('should properly parse dates', () => {
@@ -17,9 +18,15 @@ describe('ParseDateTime brick', () => {
     });
 
     it('should properly parse dates using the ISO 8601 format if no format is provided', () => {
+        const func = new ParseDateTime();
+        const context = new Context();
+        const dateStr = '2011-10-05T14:48:00.000Z';
 
-        const dateStr = '2016-01-01';
-        testEqual(new ParseDateTime(), [dateStr], [parseISO(dateStr)]);
-        testEqual(new ParseDateTime(), [dateStr, ''], [parseISO(dateStr)]);
+        const setDateSpy = jasmine.createSpy();
+
+        func.onUpdate(context, [dateStr, ''], [setDateSpy]);
+
+        // the month is 0-indexed
+        expect(setDateSpy).toHaveBeenCalledOnceWith(new Date(2011, 9, 5, 14, 48, 0, 0));
     });
 });
