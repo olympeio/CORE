7f1674351a887063d3d5304058acd13ed9b001c2
Jael Dubey <jael.dubey@olympe.ch>
2021-06-22 14:32:41 +0000

    Fix login brick

--- snapshot/dumps/Home/Olympe/Core/Auth/Login.js
+++ snapshot/dumps/Home/Olympe/Core/Auth/Login.js
@@ -44,11 +44,11 @@ const _Runnable_IO_Type_IO_0168a431d3b9b7438424     = '0168a431d3b9b7438424';
 // Definition of tags used for newly created instances 
 const _instance_of_Coded_Function_model____Login_0163d01778b931d801dd = db.assignTag('0163d01778b931d801dd');
 const _instance_of_Runnable_Input____Control_Flow_0163d01778ba31d801dd = db.assignTag('0163d01778ba31d801dd');
-const _instance_of_Runnable_Output____On_success_0163d01778bb31d801dd = db.assignTag('0163d01778bb31d801dd');
-const _instance_of_Runnable_Input____Username_0163d01798a931804d48 = db.assignTag('0163d01798a931804d48');
-const _instance_of_Runnable_Input____Password_0163d017ad3222b9683c = db.assignTag('0163d017ad3222b9683c');
+const _instance_of_Runnable_Output____on_success_0163d01778bb31d801dd = db.assignTag('0163d01778bb31d801dd');
+const _instance_of_Runnable_Input____username_0163d01798a931804d48 = db.assignTag('0163d01798a931804d48');
+const _instance_of_Runnable_Input____password_0163d017ad3222b9683c = db.assignTag('0163d017ad3222b9683c');
 const _instance_of_Runnable_Output____Error_Flow_01783bc16f72a6a3214d = db.assignTag('01783bc16f72a6a3214d');
-const _instance_of_Runnable_Output____On_failure_01783bc1d91dd3c19fa4 = db.assignTag('01783bc1d91dd3c19fa4');
+const _instance_of_Runnable_Output____on_failure_01783bc1d91dd3c19fa4 = db.assignTag('01783bc1d91dd3c19fa4');
 const _instance_of_Documentation_Function_Metadata_0179c7d30e5e4d660bb7 = db.assignTag('0179c7d30e5e4d660bb7');
 
 
@@ -70,13 +70,13 @@ const _Auth_0179b2673490430cc604               = '0179b2673490430cc604';
 
 
 // :_instance_of_Coded_Function_model____Login_0163d01778b931d801dd
-db.newModel(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, 'Login') // tag and name of the model 
-    .extends(_Function_Call_0168c6f020f2a96f0000).setModelTag(_Coded_Function_01621fb84e320000f011) 
+db.newModel(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, 'Login') // tag and name of the model
+    .extends(_Function_Call_0168c6f020f2a96f0000).setModelTag(_Coded_Function_01621fb84e320000f011)
     .setProperties({
         [_Runnable_Definition_hardcoded_01692aa71f3108454479]:true,
         'v'                                     :1
     })
-    .done(); 
+    .done();
 db.setRootInstanceTag(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd);
 // :_instance_of_Runnable_Input____Control_Flow_0163d01778ba31d801dd
 db.newInstance(_instance_of_Runnable_Input____Control_Flow_0163d01778ba31d801dd)
@@ -89,35 +89,35 @@ db.newInstance(_instance_of_Runnable_Input____Control_Flow_0163d01778ba31d801dd)
         'v'                                     :1
     })
     .done();
-// :_instance_of_Runnable_Output____On_success_0163d01778bb31d801dd
-db.newInstance(_instance_of_Runnable_Output____On_success_0163d01778bb31d801dd)
+// :_instance_of_Runnable_Output____on_success_0163d01778bb31d801dd
+db.newInstance(_instance_of_Runnable_Output____on_success_0163d01778bb31d801dd)
     .setModelTag(_Runnable_Output_0168a431d25001540002) // tag of the model
     // Tag of goos
     .addToGroupOfObjects(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd)
     .setNameOrProperties({
-        [_Object_name_ff023000000000000011]     :'On success',
+        [_Object_name_ff023000000000000011]     :'on success',
         [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:0,
         'v'                                     :1
     })
     .done();
-// :_instance_of_Runnable_Input____Username_0163d01798a931804d48
-db.newInstance(_instance_of_Runnable_Input____Username_0163d01798a931804d48)
+// :_instance_of_Runnable_Input____username_0163d01798a931804d48
+db.newInstance(_instance_of_Runnable_Input____username_0163d01798a931804d48)
     .setModelTag(_Runnable_Input_0168a431d25001540001) // tag of the model
     // Tag of goos
     .addToGroupOfObjects(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd)
     .setNameOrProperties({
-        [_Object_name_ff023000000000000011]     :'Username',
+        [_Object_name_ff023000000000000011]     :'username',
         [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:1,
         'v'                                     :1
     })
     .done();
-// :_instance_of_Runnable_Input____Password_0163d017ad3222b9683c
-db.newInstance(_instance_of_Runnable_Input____Password_0163d017ad3222b9683c)
+// :_instance_of_Runnable_Input____password_0163d017ad3222b9683c
+db.newInstance(_instance_of_Runnable_Input____password_0163d017ad3222b9683c)
     .setModelTag(_Runnable_Input_0168a431d25001540001) // tag of the model
     // Tag of goos
     .addToGroupOfObjects(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd)
     .setNameOrProperties({
-        [_Object_name_ff023000000000000011]     :'Password',
+        [_Object_name_ff023000000000000011]     :'password',
         [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:2,
         'v'                                     :1
     })
@@ -129,18 +129,18 @@ db.newInstance(_instance_of_Runnable_Output____Error_Flow_01783bc16f72a6a3214d)
     .addToGroupOfObjects(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd)
     .setNameOrProperties({
         [_Object_name_ff023000000000000011]     :'Error Flow',
-        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:1,
+        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:2,
         'v'                                     :1
     })
     .done();
-// :_instance_of_Runnable_Output____On_failure_01783bc1d91dd3c19fa4
-db.newInstance(_instance_of_Runnable_Output____On_failure_01783bc1d91dd3c19fa4)
+// :_instance_of_Runnable_Output____on_failure_01783bc1d91dd3c19fa4
+db.newInstance(_instance_of_Runnable_Output____on_failure_01783bc1d91dd3c19fa4)
     .setModelTag(_Runnable_Output_0168a431d25001540002) // tag of the model
     // Tag of goos
     .addToGroupOfObjects(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd)
     .setNameOrProperties({
-        [_Object_name_ff023000000000000011]     :'On failure',
-        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:2,
+        [_Object_name_ff023000000000000011]     :'on failure',
+        [_Runnable_IO_IO_Rank_01740c5d4ae4b520dd01]:1,
         'v'                                     :1
     })
     .done();
@@ -159,24 +159,24 @@ db.newInstance(_instance_of_Documentation_Function_Metadata_0179c7d30e5e4d660bb7
     })
     .done();
 db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____Control_Flow_0163d01778ba31d801dd);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____Username_0163d01798a931804d48);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____Password_0163d017ad3222b9683c);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Outputs_01621fb84e320000f00b , _instance_of_Runnable_Output____On_success_0163d01778bb31d801dd);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____username_0163d01798a931804d48);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____password_0163d017ad3222b9683c);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Outputs_01621fb84e320000f00b , _instance_of_Runnable_Output____on_success_0163d01778bb31d801dd);
 db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Outputs_01621fb84e320000f00b , _instance_of_Runnable_Output____Error_Flow_01783bc16f72a6a3214d);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Outputs_01621fb84e320000f00b , _instance_of_Runnable_Output____On_failure_01783bc1d91dd3c19fa4);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Runnable_Definition_Outputs_01621fb84e320000f00b , _instance_of_Runnable_Output____on_failure_01783bc1d91dd3c19fa4);
 db.assign(_instance_of_Runnable_Input____Control_Flow_0163d01778ba31d801dd, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Control_Flow_01621fb84e320000cf00                );
-db.assign(_instance_of_Runnable_Output____On_success_0163d01778bb31d801dd, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Control_Flow_01621fb84e320000cf00                );
-db.assign(_instance_of_Runnable_Input____Username_0163d01798a931804d48, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _String_ff021000000000000011                      );
-db.assign(_instance_of_Runnable_Input____Password_0163d017ad3222b9683c, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _String_ff021000000000000011                      );
+db.assign(_instance_of_Runnable_Output____on_success_0163d01778bb31d801dd, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Control_Flow_01621fb84e320000cf00                );
+db.assign(_instance_of_Runnable_Input____username_0163d01798a931804d48, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _String_ff021000000000000011                      );
+db.assign(_instance_of_Runnable_Input____password_0163d017ad3222b9683c, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _String_ff021000000000000011                      );
 db.assign(_instance_of_Runnable_Output____Error_Flow_01783bc16f72a6a3214d, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Error_Flow_016eeb5ede9f3094d8f0                  );
-db.assign(_instance_of_Runnable_Output____On_failure_01783bc1d91dd3c19fa4, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Control_Flow_01621fb84e320000cf00                );
+db.assign(_instance_of_Runnable_Output____on_failure_01783bc1d91dd3c19fa4, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Control_Flow_01621fb84e320000cf00                );
 db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Display_Metadata_$_40_$Runtime$_41_$_documentation_01703376c4a81c0e9d93, _instance_of_Documentation_Function_Metadata_0179c7d30e5e4d660bb7);
 db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____Control_Flow_0163d01778ba31d801dd);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Output____On_success_0163d01778bb31d801dd);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____Username_0163d01798a931804d48);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____Password_0163d017ad3222b9683c);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Output____on_success_0163d01778bb31d801dd);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____username_0163d01798a931804d48);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Input____password_0163d017ad3222b9683c);
 db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Output____Error_Flow_01783bc16f72a6a3214d);
-db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Output____On_failure_01783bc1d91dd3c19fa4);
+db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Runnable_Output____on_failure_01783bc1d91dd3c19fa4);
 db.assign(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_containsRel_ff02200000000000000f          , _instance_of_Documentation_Function_Metadata_0179c7d30e5e4d660bb7);
 db.assignOptional(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _Object_gooRel_ff022000000000000003, _Auth_0179b2673490430cc604);
 db.assignOptional(_instance_of_Coded_Function_model____Login_0163d01778b931d801dd, _GOO_parentGooRel_ff02200000000000000u, _Auth_0179b2673490430cc604);
