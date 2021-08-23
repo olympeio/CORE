274003433bca16e2f630a67c5f7b3b587526ba7f
Syméon del Marmol <symeon.delmarmol@olympe.ch>
2021-08-23 10:37:36 +0200

    Hotfix: remove complex property input default value for CreateLocalObject.

--- snapshot/dumps/Home/Olympe/Core/Data/Transaction/Create_Local_Object.js
+++ snapshot/dumps/Home/Olympe/Core/Data/Transaction/Create_Local_Object.js
@@ -15,7 +15,6 @@ const _Documentation_01703376c4a81c0e9d90      = '01703376c4a81c0e9d90';
 const _Function_Call_0168c6f020f2a96f0000      = '0168c6f020f2a96f0000';
 const _Model_type_0162f672366be5ca7a86         = '0162f672366be5ca7a86';
 const _Object_ff021000000000000001             = 'ff021000000000000001';
-const _ObjectRef_0163c9ae01f2d6e9ca49          = '0163c9ae01f2d6e9ca49';
 const _Predefined_input_Control_Flow_of_Action_016ac63229782039429f = '016ac63229782039429f';
 const _Predefined_output_Control_Flow_016ac6322acf67eebfe7 = '016ac6322acf67eebfe7';
 const _Runnable_Input_0168a431d25001540001     = '0168a431d25001540001';
@@ -40,7 +39,6 @@ const _Folder_itemRel_016a697ef00ce7554692          = '016a697ef00ce7554692';
 const _GOO_parentGooRel_ff02200000000000000u        = 'ff02200000000000000u';
 const _Object_containsRel_ff02200000000000000f      = 'ff02200000000000000f';
 const _Object_gooRel_ff022000000000000003           = 'ff022000000000000003';
-const _ObjectRef_reference_0169b5a8a33144fb0b49     = '0169b5a8a33144fb0b49';
 const _Runnable_Definition_Inputs_01621fb84e320000f00a = '01621fb84e320000f00a';
 const _Runnable_Definition_Outputs_01621fb84e320000f00b = '01621fb84e320000f00b';
 const _Runnable_IO_Type_IO_0168a431d3b9b7438424     = '0168a431d3b9b7438424';
@@ -55,7 +53,6 @@ const _instance_of_Runnable_Output____object_0162f2eaea705e367590 = db.assignTag
 const _instance_of_Documentation_017b35893b6906f58632 = db.assignTag('017b35893b6906f58632');
 const _instance_of_Documentation_017b35893f661288be44 = db.assignTag('017b35893f661288be44');
 const _instance_of_Documentation_017b35894278d9ba0e38 = db.assignTag('017b35894278d9ba0e38');
-const _instance_of_ObjectRef_017b5afc6c10651f886f   = db.assignTag('017b5afc6c10651f886f');
 
 
 //==========================================================
@@ -165,17 +162,6 @@ db.newInstance(_instance_of_Documentation_017b35894278d9ba0e38)
         'v'                                     :1
     })
     .done();
-// :_instance_of_ObjectRef_017b5afc6c10651f886f
-db.newInstance(_instance_of_ObjectRef_017b5afc6c10651f886f)
-    .setModelTag(_ObjectRef_0163c9ae01f2d6e9ca49) // tag of the model
-    .setNameOrProperties({
-        'v'                                     :1
-    })
-    .done();
-db.updateInstance(_instance_of_Runnable_Input____model_0162f2eab3eadc7dff16, {
-        [_Runnable_Input_Default_Value_01740c5d4ae4b520dd02]:db.createReference(_instance_of_ObjectRef_017b5afc6c10651f886f),
-        'v'                                     :3
-    });
 db.assign(_instance_of_Coded_Action_model____Create_Local_Object_0162f2e963b6c9ed85b7, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Predefined_input_Control_Flow_of_Action____Control_Flow_0162f2e963b7c9ed85b7);
 db.assign(_instance_of_Coded_Action_model____Create_Local_Object_0162f2e963b6c9ed85b7, _Runnable_Definition_Inputs_01621fb84e320000f00a  , _instance_of_Runnable_Input____model_0162f2eab3eadc7dff16);
 db.assign(_instance_of_Coded_Action_model____Create_Local_Object_0162f2e963b6c9ed85b7, _Runnable_Definition_Outputs_01621fb84e320000f00b , _instance_of_Predefined_output_Control_Flow____Control_Flow_0162f2e963b8c9ed85b7);
@@ -184,7 +170,6 @@ db.assign(_instance_of_Predefined_input_Control_Flow_of_Action____Control_Flow_0
 db.assign(_instance_of_Predefined_output_Control_Flow____Control_Flow_0162f2e963b8c9ed85b7, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Control_Flow_01621fb84e320000cf00                );
 db.assign(_instance_of_Runnable_Input____model_0162f2eab3eadc7dff16, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Model_type_0162f672366be5ca7a86                  );
 db.assign(_instance_of_Runnable_Output____object_0162f2eaea705e367590, _Runnable_IO_Type_IO_0168a431d3b9b7438424         , _Object_ff021000000000000001                      );
-db.assign(_instance_of_ObjectRef_017b5afc6c10651f886f       , _ObjectRef_reference_0169b5a8a33144fb0b49         , _String_ff021000000000000011                      );
 db.assign(_instance_of_Documentation_017b35893b6906f58632   , _Documentation_documentation_01703376c4a81c0e9d93 , _instance_of_Coded_Action_model____Create_Local_Object_0162f2e963b6c9ed85b7);
 db.assign(_instance_of_Documentation_017b35893f661288be44   , _Documentation_documentation_01703376c4a81c0e9d93 , _instance_of_Runnable_Input____model_0162f2eab3eadc7dff16);
 db.assign(_instance_of_Documentation_017b35894278d9ba0e38   , _Documentation_documentation_01703376c4a81c0e9d93 , _instance_of_Runnable_Output____object_0162f2eaea705e367590);
