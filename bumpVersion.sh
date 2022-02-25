d5bf56db271831455500571e002cacbe6e613a37
Syméon del Marmol <symeon.delmarmol@olympe.ch>
2022-02-25 19:15:17 +0100

    Add timestamp in dev version for published package

--- bumpVersion.sh
+++ bumpVersion.sh
@@ -2,9 +2,8 @@
 export PACKAGE='@olympeio/core'
 export VERSION=$(jq -rc '.version' ./package.json)
 export COMMIT_REF=$(git log -n 1 --pretty=format:"%h")
-export PUBLISH_VER="${VERSION}-${COMMIT_REF}"
+export TIMESTAMP=$(date +%s)
+export PUBLISH_VER="${VERSION}-${COMMIT_REF}-${TIMESTAMP}"
 
 echo "Setting npm packages version to ${PUBLISH_VER}"
 sed -i -e "s/\"version\": \".*\",/\"version\": \"${PUBLISH_VER}\",/" package.json
-
-
