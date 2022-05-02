408b26be82196b3a20712c4641259c95835f89be
Jean-Christophe Collet <jean-christophe.collet@olympe.ch>
2022-05-02 18:23:42 +0200

    Release 2.0.3

--- setVersion.sh
+++ setVersion.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 if [ "$1" = "-h" ]
 then
-  echo "usage: setVersion [<version> [<framework>]]"
+  echo "usage: setVersion [<version> [<framework> [dev-tools]]]"
   exit 1
 fi
 FILES="package.json"
 export VERSION=$1
 export FW_VERSION=$2
+export DEVTOOLS_VERSION=$3
 if [ -z "$VERSION" ]
 then
   read -p "Core version: " -r VERSION
@@ -17,20 +18,21 @@ then
   read -p "Framework version: " -r FW_VERSION
 fi
 
+if [ -z "$DEVTOOLS_VERSION" ]
+then
+  read -p "Dev-Tools version: " -r DEVTOOLS_VERSION
+fi
+
 echo "setting versions to:"
 echo "    Core: ${VERSION}"
 echo "    Framework: ${FW_VERSION}"
-
-export FW_GIT_TAG=$FW_VERSION
-if [[ $FW_VERSION =~ ^[0-9]+.[0-9]+.[0-9]+.*$ ]]
-then
-  export FW_GIT_TAG="v${FW_VERSION}"
-fi
+echo "    DevTools: ${DEVTOOLS_VERSION}"
 
 # Core version
 for f in $FILES
 do
   sed -i '' -e "s/\"version\": \".*\",/\"version\": \"$VERSION\",/" "$f"
-  sed -i '' -e "s/\"@olympeio\/runtime-\(.*\)\": \".*\",/\"@olympeio\/runtime-\\1\": \"$VERSION\",/" "$f"
+  sed -i '' -e "s/\"@olympeio\/dev-tools\": \".*\",/\"@olympeio\/dev-tools\": \"$DEVTOOLS_VERSION\",/" "$f"
+  sed -i '' -e "s/\"@olympeio\/runtime-\(.*\)\": \".*\",/\"@olympeio\/runtime-\\1\": \"$FW_VERSION\",/" "$f"
 done
 
