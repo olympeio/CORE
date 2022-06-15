63dc9953a5662acb2a1ec97a4133925a59b84b21
Syméon del Marmol <symeon.delmarmol@olympe.ch>
2022-06-15 15:57:46 +0000

    CORE-135: Simplify and fix JSONToCloudObject brick to handle relations correctly

--- src/core/data/queries/ExecuteQueryFromCache.js
+++ src/core/data/queries/ExecuteQueryFromCache.js
@@ -1,4 +1,20 @@
-import { ActionBrick, registerBrick, ErrorFlow } from 'olympe';
+/**
+ * Copyright 2021 Olympe S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ActionBrick, registerBrick } from 'olympe';
 
 export default class ExecuteQueryFromCache extends ActionBrick {
 
@@ -9,16 +25,10 @@ export default class ExecuteQueryFromCache extends ActionBrick {
      * @param {!Query} query
      * @param {function()} forwardEvent
      * @param {function(!QueryResult)} setQueryResult
-     * @param {function(string)} setErrorFlow
      */
-    update($, [query], [forwardEvent, setQueryResult, setErrorFlow]) {
-        try {
-            setQueryResult(query.executeFromCache());
-        } catch(e) {
-            setErrorFlow(ErrorFlow.create(e, 1));
-        } finally {
-            forwardEvent();
-        }
+    update($, [query], [forwardEvent, setQueryResult]) {
+        setQueryResult(query.executeFromCache());
+        forwardEvent();
     }
 }
 
