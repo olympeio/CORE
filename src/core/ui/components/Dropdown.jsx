253a2f22f80d85b4bf1a76073616c7a7b4a79077
Sofiia Chorna <sofiia.chorna@olympe.io>
2022-07-13 16:21:37 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/core/ui/components/Dropdown.jsx
+++ src/core/ui/components/Dropdown.jsx
@@ -393,7 +393,7 @@ export default class Dropdown extends ReactBrick {
         const findOptionValue = (value) => {
             const foundOption = options.find(opt => {
                 // Has to filter differently if it's cloud objects
-                if(opt.value.getTag){
+                if(opt.value instanceof CloudObject){
                     return opt.value.getTag() === value?.getTag()
                 } else {
                     return opt.value === value
